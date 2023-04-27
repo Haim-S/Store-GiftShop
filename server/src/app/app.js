6579bdf5f9d530c3c00012e272aab3dfe4753a75
HaimSlbah <26life72@gmail.com>
2023-04-27 18:17:37 +0300

    fix the server

--- server/src/app/app.js
+++ server/src/app/app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+const routes = require("../routes/index")
+
 app.use(express.urlencoded({extended: false}));
 app.use(cors({
     origin: [
@@ -10,7 +12,7 @@ app.use(cors({
     ]
 }));
 app.use(express.json());
-// app.use("/", routes);
+app.use("/", routes);
 
 
 module.exports = app;
